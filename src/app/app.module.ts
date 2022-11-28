edd65bd537c4f140705400265005d641bf7abbf6
Pedro Conde <pconde007@gmail.com>
2022-11-28 14:02:09 +0000

    Se crea la aplicación de fronend para las gráficas de barras en tiempo real

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
+import { HttpClientModule } from '@angular/common/http';
+
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+ 
+const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
+
+
 
 import { AppComponent } from './app.component';
+import { EncuestaComponent } from './components/encuesta/encuesta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncuestaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule,
+    HttpClientModule,
+    SocketIoModule.forRoot( config )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/encuesta/encuesta.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { WebsocketService } from '../../services/websocket.service';
+
+@Component({
+  selector: 'app-encuesta',
+  templateUrl: './encuesta.component.html',
+  styleUrls: ['./encuesta.component.css']
+})
+export class EncuestaComponent implements OnInit {
+
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+   };
+
+
+  public barChartLabels: string[] = ['Pregunta 1', 'Pregunta 2', 'Pregunta 4', 'Pregunta 4'];
+  public barChartType = 'bar';
+
+  public barChartData: any[] = [
+    {data: [ 65, 59, 80, 81 ], label: 'Entrevistados'}
+  ];
+
+
+  constructor(
+    private http: HttpClient,
+    public wsService: WebsocketService
+  ) { }
+
+  ngOnInit() {
+    this.http.get('http://localhost:5000/grafica')
+      .subscribe( (data: any) => this.barChartData = data );
+
+    this.escucharSocket();
+  }
+
+  escucharSocket() {
+
+    this.wsService.listen('cambio-grafica')
+      .subscribe( (data: any) => this.barChartData = data );
+
+  }
+
+}
